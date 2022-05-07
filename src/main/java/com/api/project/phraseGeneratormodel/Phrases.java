492dfd69a15cb5ff2873720842e5aa0dca314286
Mário Junior <48899365+Mario23junior@users.noreply.github.com>
2022-05-07 04:31:59 -0300

    Criando modelo de dados de teste

---
+++ src/main/java/com/api/project/phraseGeneratormodel/Phrases.java
@@ -0,0 +1,46 @@
+package com.api.project.phraseGeneratormodel;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Phrases {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	private String phrases;
+	private String author;
+	
+	public Phrases() {
+		//TODO Auto-generated constructor stub
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getPhrases() {
+		return phrases;
+	}
+
+	public void setPhrases(String phrases) {
+		this.phrases = phrases;
+	}
+
+	public String getAuthor() {
+		return author;
+	}
+
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+	
+	
+}
