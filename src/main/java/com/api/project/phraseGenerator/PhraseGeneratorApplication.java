12561cf2bc02eac4fc755e21a6cb19b5ae904489
Mário Junior <48899365+Mario23junior@users.noreply.github.com>
2022-05-07 04:21:59 -0300

    Iniciando projeto gerador de frases cinetificas

---
+++ src/main/java/com/api/project/phraseGenerator/PhraseGeneratorApplication.java
@@ -0,0 +1,13 @@
+package com.api.project.phraseGenerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhraseGeneratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhraseGeneratorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/api/project/phraseGenerator/PhraseGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.project.phraseGenerator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhraseGeneratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
